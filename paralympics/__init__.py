5041909618e316b06e3379033299247a41b782a1
01711-01711 <mateuszzawila01@gmail.com>
2024-02-17 13:56:59 +0000

    change

--- paralympics/__init__.py
+++ paralympics/__init__.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
+from flask_marshmallow import Marshmallow
 
 
 # https://flask-sqlalchemy.palletsprojects.com/en/3.1.x/quickstart/
@@ -10,9 +11,10 @@ class Base(DeclarativeBase):
     pass
 
 
-# First create the db object using the SQLAlchemy constructor.
-# Pass a subclass of either DeclarativeBase or DeclarativeBaseNoMeta to the constructor.
-db = SQLAlchemy(model_class=Base)
+# Create a global SQLAlchemy object
+db = SQLAlchemy()
+# Create a global Flask-Marshmallow object
+ma = Marshmallow()
 
 
 def create_app(test_config=None):
@@ -42,6 +44,8 @@ def create_app(test_config=None):
 
     # Initialise Flask with the SQLAlchemy database extension
     db.init_app(app)
+
+    ma.init_app(app)
 
     # Models are defined in the models module, so you must import them before calling create_all, otherwise SQLAlchemy
     # will not know about them.
--- paralympics/paralympics.py
+++ paralympics/paralympics.py
@@ -1,6 +1,84 @@
+'''
 from flask import current_app as app
-
-
 @app.route('/')
 def hello():
     return f"Hello!"
+'''
+
+
+from flask import Blueprint, request, jsonify
+from . import db, ma
+from .models import Event, Region
+from .schemas import EventSchema, RegionSchema
+
+bp = Blueprint('paralympics', __name__)
+
+# Flask-Marshmallow Schemas
+regions_schema = RegionSchema(many=True)
+region_schema = RegionSchema()
+events_schema = EventSchema(many=True)
+event_schema = EventSchema()
+
+@bp.route("/regions", methods=["GET"])
+def get_regions():
+    all_regions = Region.query.all()
+    result = regions_schema.dump(all_regions)
+    return jsonify(result)
+
+@bp.route("/regions/<NOC>", methods=["GET"])
+def get_region(NOC):
+    region = Region.query.get_or_404(NOC)
+    return region_schema.jsonify(region)
+
+@bp.route("/regions", methods=["POST"])
+def add_region():
+    new_region = region_schema.load(request.json, session=db.session)
+    db.session.add(new_region)
+    db.session.commit()
+    return region_schema.jsonify(new_region), 201
+
+@bp.route("/regions/<NOC>", methods=["PATCH"])
+def update_region(NOC):
+    region = Region.query.get_or_404(NOC)
+    region_schema.load(request.json, instance=region, session=db.session, partial=True)
+    db.session.commit()
+    return region_schema.jsonify(region)
+
+@bp.route("/regions/<NOC>", methods=["DELETE"])
+def delete_region(NOC):
+    region = Region.query.get_or_404(NOC)
+    db.session.delete(region)
+    db.session.commit()
+    return jsonify({"message": f"Region {NOC} successfully deleted."}), 202
+
+@bp.route("/events", methods=["GET"])
+def get_events():
+    all_events = Event.query.all()
+    result = events_schema.dump(all_events)
+    return jsonify(result)
+
+@bp.route("/events/<int:event_id>", methods=["GET"])
+def get_event(event_id):
+    event = Event.query.get_or_404(event_id)
+    return event_schema.jsonify(event)
+
+@bp.route("/events", methods=["POST"])
+def add_event():
+    new_event = event_schema.load(request.json, session=db.session)
+    db.session.add(new_event)
+    db.session.commit()
+    return event_schema.jsonify(new_event), 201
+
+@bp.route("/events/<int:event_id>", methods=["PATCH"])
+def update_event(event_id):
+    event = Event.query.get_or_404(event_id)
+    event_schema.load(request.json, instance=event, session=db.session, partial=True)
+    db.session.commit()
+    return event_schema.jsonify(event)
+
+@bp.route("/events/<int:event_id>", methods=["DELETE"])
+def delete_event(event_id):
+    event = Event.query.get_or_404(event_id)
+    db.session.delete(event)
+    db.session.commit()
+    return jsonify({"message": f"Event {event_id} successfully deleted."}), 202
